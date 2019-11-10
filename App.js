802896a556ee1a313e1dc9195ffbbf7276aeeda7
manandearth <adamgefen@posteo.net>
2019-11-10 20:24:10 +0100

    Add `Location Data` component, permission and logic.

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LocationData } from './LocationData';
 
 export default function App() {
+  // return (
+  // <View style={styles.container}>
+  // <Text>Robin is a NP (Ninkam-poop)</Text>
+  // </View>
+  // );
+
   return (
-    <View style={styles.container}>
-      <Text>Robin is a NP (Ninkam-poop)</Text>
-    </View>
+    <LocationData />
   );
 }
 
---
+++ LocationData.js
@@ -0,0 +1,43 @@
+
+import React, { useState, Component } from 'react';
+import {
+  Platform, Text, View, StyleSheet,
+} from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+export const LocationData = () => {
+  const [location, setLocation] = useState({ lon: 11, lat: 15 });
+
+  const getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      setLocation({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+    const reading = await Location.getCurrentPositionAsync({});
+    setLocation({ reading });
+  };
+  getLocationAsync();
+  return (
+    <View style={styles.container}>
+      <Text>
+        {
+          location.errorMessage ? location.errorMessage : JSON.stringify(location)
+}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    color: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
