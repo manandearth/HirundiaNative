a1602c6e85e99e980bb0909d524c4e14396e935e
manandearth <adamgefen@posteo.net>
2019-11-10 21:12:40 +0100

    Add error and waiting to `LocationData` state

--- LocationData.js
+++ LocationData.js
@@ -1,4 +1,3 @@
-
 import React, { useState, Component } from 'react';
 import {
   Platform, Text, View, StyleSheet,
@@ -8,7 +7,7 @@ import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
 export const LocationData = () => {
-  const [location, setLocation] = useState({ lon: 11, lat: 15 });
+  const [location, setLocation] = useState({ state: 'waiting...' });
 
   const getLocationAsync = async () => {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
@@ -21,23 +20,23 @@ export const LocationData = () => {
     setLocation({ reading });
   };
   getLocationAsync();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      color: 'red',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
   return (
     <View style={styles.container}>
       <Text>
-        {
-          location.errorMessage ? location.errorMessage : JSON.stringify(location)
-}
+        {location.errorMessage ? location.errorMessage
+          : location.state ? location.state : JSON.stringify(location)}
       </Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    color: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
