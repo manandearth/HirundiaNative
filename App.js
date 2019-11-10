356464d18ce8bae7c6774e9b6a3beb38a70d143e
manandearth <adamgefen@posteo.net>
2019-11-10 18:45:12 +0100

    Add Installing 1 SDK 35.0.0 compatible native module using npm. > npm install audited 37602 packages in 4.275s found 11 low severity vulnerabilities   run `npm audit fix` to fix them, or `npm audit` for details

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Robin is a NP (Ninkam-poop)</Text>
     </View>
   );
 }
