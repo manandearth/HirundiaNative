6c3574374117ae0b1640ab64e868ce89a06caf14
manandearth <adamgefen@posteo.net>
2019-11-24 22:51:06 +0100

    Add Pickers

--- form.js
+++ form.js
@@ -37,7 +37,7 @@ export const Form = () => {
     const heights = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18'];
     return(<View style={styles.view}>
              <Text style={styles.text}>Height: </Text>
-             <Picker style={{width: 100}}
+             <Picker style={{width: 120}}
                      selectedValue={height}
                      onValueChange={(itemValue, itemIndex) => setHeight(itemValue)}
                      mode='dialog'>
@@ -45,12 +45,42 @@ export const Form = () => {
              </Picker>
            </View>);
   };
+   const Type = () => {
+    const [type, setType ] = useState('1');
+     const types= ['window', 'crack', 'gable', 'balcony', 'cornice', 'cables', ];
+    return(<View style={styles.view}>
+             <Text style={styles.text}>Type: </Text>
+             <Picker style={{width: 120}}
+                     selectedValue={type}
+                     onValueChange={(itemValue, itemIndex) => setType(itemValue)}
+                     mode='dialog'>
+               {types.map(n => <Picker.Item key={'key-' + n} label={n} value={n}/>)}
+             </Picker>
+           </View>);
+   };
+    const Species = () => {
+    const [species, setSpecies ] = useState('1');
+      const specieses= ['Swallow', 'Swift', 'Martin' ];
+    return(<View style={styles.view}>
+             <Text style={styles.text}>Species: </Text>
+             <Picker style={{width: 120}}
+                     selectedValue={species}
+                     onValueChange={(itemValue, itemIndex) => setSpecies(itemValue)}
+                     mode='dialog'>
+               {specieses.map(n => <Picker.Item key={'key-' + n} label={n} value={n}/>)}
+             </Picker>
+           </View>);
+   };
+  
   
   return(<View>
            <StreetInput />
            <HouseNumberInput />
            <LocationData />
+           <Species/>
+           <Type />
            <Height />
+           
          </View>);
 };
 
